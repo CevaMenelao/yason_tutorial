bb15a23f878f0f40f3cccb648b4f8cefad7b87f2
CevaMenelao <38533197+CevaMenelao@users.noreply.github.com>
2020-09-03 20:28:22 -0500

    Fisrt Commit

---
+++ src/Lucid/Yesod_ejemplo1.hs
@@ -0,0 +1,25 @@
+{-# LANGUAGE ExtendedDefaultRules   #-}
+{-# LANGUAGE OverloadedStrings      #-}
+{-# LANGUAGE NoImplicitPrelude      #-}
+{-# LANGUAGE FlexibleInstances      #-}
+{-# LANGUAGE ScopedTypeVariables    #-}
+
+module Lucid.Yesod_ejemplo1 where
+
+--import           Import hiding ((==.), for_, toHtml, Builder)
+import           Import hiding ((==.), for_, Html, toHtml, Builder)
+import qualified Lucid.Base as LucidBase
+import           Lucid
+import           Lucid.Supplemental
+import qualified Blaze.ByteString.Builder as Blaze
+import qualified Blaze.ByteString.Builder.Html.Utf8 as Blaze
+import           Blaze.ByteString.Builder (Builder)
+import System.IO (stdout, hSetEncoding, utf8)
+import Data.Text.Lazy.IO as L
+import           Database.Persist.Sql ( fromSqlKey )
+
+holamundo :: Html ()
+holamundo = do
+    div_ [] $ do
+        p_ [] "Hola mundo"
+
